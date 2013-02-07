f3ae090db12ee19e9cf2576f241ebe8545231e9a
Mofeed Hassan <mofeed@ubuntu.(none)>
2013-02-07 15:40:34 +0100

    SERIMI

---
+++ Jrubygemsacoording toSERIMI/geminstall.sh
@@ -0,0 +1,22 @@
+
+gem install actionmailer-2.3.2.gem
+gem install actionpack-2.3.2.gem
+gem install activerecord-2.3.2.gem
+gem install activeresource-2.3.2.gem
+gem install activesupport-2.3.2.gem
+gem install amatch-0.2.5.gem
+gem install bouncy-castle-java-1.5.0146.1.gem 
+gem install elasticsearch-0.0.0.gem
+gem install jruby-openssl-0.7.4.gem
+gem install json-1.5.1.gem
+gem install OptionParser-0.5.1.gem  
+gem install rails-2.3.2.gem
+gem install rake-0.8.7.gem
+gem install sources-0.0.1.gem
+gem install Text-1.1.2.gem
+gem install text-1.2.1.gem
+gem install uuidtools-1.0.7.gem
+gem install xml-object-0.9.93.gem
+gem install yajl-ruby-0.8.2.gem
+    jruby-launcher (1.0.7 java)
+    patron (0.4.9)
